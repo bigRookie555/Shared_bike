c4899e4913ec350aac76b88743b2efa971f27727
‘skx’ <kxshao@126.com>
2022-03-06 14:27:32 +0800

    log4cpp 1

---
+++ src/common/Logger.h
@@ -0,0 +1,13 @@
+#ifndef _SHBK_COMMON_LOGGER_H_
+#define _SHBK_COMMON_LOGGER_H_
+
+#include<string>
+#include<log4cpp/Category.hh>
+
+class Logger{
+public:
+	bool init(const std::string)
+};
+
+
+#endif
